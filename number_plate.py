f8a27dc03b47ce1744d18ac5f59d1183aa9c953a
KAM1CODER <92083646+KAM1CODER@users.noreply.github.com>
2023-08-03 12:34:39 +0530

    latest

--- number_plate.py
+++ number_plate.py
@@ -1,2 +1,13 @@
 import cv2
-cv2.imread
+face_cascade = cv2.CascadeClassifier('./haarcascade_russian_plate_number.xml')
+img = cv2.imread('./assets/car.JPG')
+# Convert into grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# Detect faces
+faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+# Draw rectangle around the faces
+for (x, y, x1, y1) in faces:
+    cv2.rectangle(img, (x, y), (x1, y1), (255, 0, 0), 2)
+# Display the output
+cv2.imshow('img', img)
+cv2.waitKey()
